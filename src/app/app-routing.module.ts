19a231387f41d54a317722ac8e75115ccdd60d08
Matheus Andrade <matheus.andrade@capgemini.com>
2024-02-26 22:28:11 -0300

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login'
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'login',
